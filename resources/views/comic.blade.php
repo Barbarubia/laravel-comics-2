bf1d4992b5f953e797f3ca5b086e0fd5e8c75bd1
Barbarubia <dnl.desimone@gmail.com>
2022-04-29 16:47:14 +0200

    thumb fumetto nella route comic/id

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,6 +3,17 @@
 @section('title', 'Comic')
 
 @section('content')
+
+    <section class="thumb-comic">
+        <div class="container-narrow">
+            <div class="thumb-container">
+                <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                <span class="label label-comic-book">Comic Book</span>
+                <span class="label label-view-gallery">View Gallery</span>
+            </div>
+        </div>
+    </section>
+
     <section class="main-content">
         <div class="container-narrow">
             <h1>{{ $comic['title'] }}</h1>
